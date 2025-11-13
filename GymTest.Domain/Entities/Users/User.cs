5d40c45e0d4f43b19452eb7dff17cc44e1d901b8
rezanaim <reza.naim1380@gmail.com>
2025-11-13 18:37:43 +0330

    User 1

--- GymTest.Domain/Entities/Users/User.cs
+++ GymTest.Domain/Entities/Users/User.cs
@@ -8,12 +8,14 @@ using System.Threading.Tasks;
 
 namespace GymTest.Domain.Entities.Users
 {
-    public class User : BaseEntity
+    public class User //: BaseEntity
     {
-        public string FullName { get; set; }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public bool IsActive { get; set; }
+        //public bool IsActive { get; set; }
         public string Role { get; set; }
 
 
