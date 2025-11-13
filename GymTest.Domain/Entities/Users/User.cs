bf3a5409e581d87eabe0188985858bdba2467154
rezanaim <reza.naim1380@gmail.com>
2025-11-13 22:46:36 +0330

    User add Role

--- GymTest.Domain/Entities/Users/User.cs
+++ GymTest.Domain/Entities/Users/User.cs
@@ -16,8 +16,9 @@ namespace GymTest.Domain.Entities.Users
         public string Email { get; set; }
         public string Password { get; set; }
         //public bool IsActive { get; set; }
-        public string Role { get; set; }
+        public string Role { get; set; } = "Customer";
 
+        
 
     }
 }
