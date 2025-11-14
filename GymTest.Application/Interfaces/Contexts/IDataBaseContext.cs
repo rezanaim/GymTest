e10e2acc77a7f1e3022e2380d705e45c4f83aa66
rezanaim <reza.naim1380@gmail.com>
2025-11-14 18:57:49 +0330

    add IDataBaseContext

---
+++ GymTest.Application/Interfaces/Contexts/IDataBaseContext.cs
@@ -0,0 +1,20 @@
+﻿using GymTest.Domain.Entities.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace GymTest.Application.Interfaces.Contexts
+{
+    interface IDataBaseContext
+    {
+        DbSet<User> Users { get; set; }
+
+
+    }
+}
