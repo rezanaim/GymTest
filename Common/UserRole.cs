2a5c81a3add63be42abd68109dcc4755c4329d60
rezanaim <reza.naim1380@gmail.com>
2025-11-14 17:35:06 +0330

    UserRole in Comon

---
+++ Common/UserRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymTest.Common
+{
+    class UserRole
+    {
+        public const string Admin = "Admin";
+        public const string Coach = "Coach";
+        public const string Customer = "Customer";
+    }
+}
