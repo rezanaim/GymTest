6be67a3279c8d84f5795ef5aaa50ccdb610f6c20
rezanaim <reza.naim1380@gmail.com>
2025-11-16 03:00:27 +0330

    add RoleId property to UserInRole class

--- GymTest.Application/Services/Users/Command/IRegisterUser.cs
+++ GymTest.Application/Services/Users/Command/IRegisterUser.cs
@@ -1,10 +1,12 @@
 ﻿using Common;
 using GymTest.Application.Interfaces.Contexts;
+using GymTest.Common;
 using GymTest.Domain.Entities.Users;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using static GymTest.Application.Services.Users.Command.UserRegister;
 
@@ -24,11 +26,124 @@ namespace GymTest.Application.Services.Users.Command
         }
         public ResultDto<ResultRegisterUserto> Execute(RequestRegisterUserDto request)
         {
-            //موقتا  ریترن نال
-            return null;
-        }
 
-        public class RequestRegisterUserDto
+            if (string.IsNullOrWhiteSpace(request.Email))
+            {
+                return new ResultDto<ResultRegisterUserto>()
+                {
+                    Data = new ResultRegisterUserto()
+                    {
+                        UserId = 0,
+                    },
+                    IsSuccess = false,
+                    Message = "لطفا ایمیل خود را وارد کنید"
+                };
+            }
+
+            if (string.IsNullOrWhiteSpace(request.FirstName))
+            {
+                return new ResultDto<ResultRegisterUserto>()
+                {
+                    Data = new ResultRegisterUserto()
+                    {
+                        UserId = 0,
+                    },
+                    IsSuccess = false,
+                    Message = "لطفا نام خود را وارد کنید"
+                };
+            }
+
+            if (string.IsNullOrWhiteSpace(request.LastName))
+            {
+                return new ResultDto<ResultRegisterUserto>()
+                {
+                    Data = new ResultRegisterUserto()
+                    {
+                        UserId = 0,
+                    },
+                    IsSuccess = false,
+                    Message = "لطفا نام خانوادگی خود را وارد کنید"
+                };
+            }
+
+            if (string.IsNullOrWhiteSpace(request.Password))
+            {
+                new ResultDto<ResultRegisterUserto>()
+                {
+                    Data = new ResultRegisterUserto()
+                    {
+                        UserId = 0,
+                    },
+                    IsSuccess = false,
+                    Message = "لطفا یک پسورد انتخاب کنید"
+                };
+            }
+
+            if (request.Password != request.RePassword)
+            {
+                new ResultDto<ResultRegisterUserto>()
+                {
+                    Data = new ResultRegisterUserto()
+                    {
+                        UserId = 0,
+                    },
+                    IsSuccess = false,
+                    Message = "پسورد و تکرار آن، یکسان نیستند"
+
+                };
+            }
+            // چک فرمت ایمیل
+            string emailRegex = @"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$";
+            var match = Regex.Match(request.Email, emailRegex, RegexOptions.IgnoreCase);
+
+            if (!match.Success)
+            {
+                return new ResultDto<ResultRegisterUserto>()
+                {
+                    Data = new ResultRegisterUserto()
+                    {
+                        UserId = 0,
+                    },
+                    IsSuccess = false,
+                    Message = "لطفا یک ایمیل معتبر وارد کنید"
+                };
+            }
+
+            var passwordHasher = new PasswordHasher();
+            var hashedPassword = passwordHasher.HashPassword(request.Password);
+            var User = new User()
+            {
+                Email = request.Email,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Password = hashedPassword,
+                IsActive = true,
+            };
+
+            var userInRoles = new List<UserInRole>();
+
+            foreach (var item in userInRoles)
+            {
+                var roles = _context.Roles.Find();
+                userInRoles.Add(new UserInRole()
+                {
+                    Role = Roles,
+                    RoleId = roles.Id,
+                    User = User,
+                    UserId = User.Id,
+
+                });
+            }
+            User.UserInRoles = UserInRoles
+
+
+
+        }
+        //موقتا  ریترن نال
+
+    }
+
+    public class RequestRegisterUserDto
         {
             public string Email { get; set; }
             public string FirstName { get; set; }
@@ -45,7 +160,7 @@ namespace GymTest.Application.Services.Users.Command
 
         public class ResultRegisterUserto
         {
-            public string UserId { get; set; }
+            public long UserId { get; set; }
         }
     }
-}
+
--- GymTest.Domain/Entities/Users/UserInRole.cs
+++ GymTest.Domain/Entities/Users/UserInRole.cs
@@ -5,7 +5,7 @@ namespace GymTest.Domain.Entities.Users
 {
     public class UserInRole : BaseEntity
     {
-        //public long Id { get; set; }
+        public long RoleId { get; set; }
 
         // Relation with User Table
         public virtual User User { get; set; }
