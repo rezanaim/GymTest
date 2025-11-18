5ba3777869053617c80c7c57108e51ad0037754f
rezanaim <reza.naim1380@gmail.com>
2025-11-18 11:31:06 +0330

    Public class UserRoles

---
+++ Common/UserRoles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymTest.Common
+{
+    public class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string Coach = "Coach";
+        public const string Customer = "Customer";
+    }
+}
