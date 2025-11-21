3e32bfdca50aec4b1bd1aa862f114e82439e71ee
rezanaim <reza.naim1380@gmail.com>
2025-11-21 10:23:08 +0330

    base Index UserController

---
+++ EndPoint.Site/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,102 @@
+﻿using GymTest.Application.Services.Users.Query;
+using GymTest.Application.Services.Users.Query.GetUser;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EndPoint.Site.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IGetUsers _getUsers;
+        public UserController(IGetUsers getUsers)
+        {
+            _getUsers = getUsers;
+        }
+
+
+        // GET: UserController1
+        public ActionResult Index(string searchKey = "", int pageNumber = 1)
+        {
+            var request = new RequestGetUserDto()
+            {
+                PageNumber = pageNumber,
+                SearchKey = searchKey
+            };
+            var result = _getUsers.Execute(request);
+            return View(result);
+        }
+
+        // GET: UserController1/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UserController1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserController1/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserController1/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UserController1/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserController1/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UserController1/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
