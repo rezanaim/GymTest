fe4ddfc5d573d972691b47c9e09c448e3d161c5d
rezanaim <reza.naim1380@gmail.com>
2025-11-23 14:55:25 +0330

    add Course/Sport/Category?UserInCourse Classes

--- GymTest.Domain/Entities/Course/Course.cs
+++ GymTest.Domain/Entities/Course/Course.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GymTest.Domain.Entities.Commons;
+using GymTest.Domain.Entities.Users;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,13 +8,80 @@ using System.Threading.Tasks;
 
 namespace GymTest.Domain.Entities.Course
 {
-    public class Course
+    public class Course : BaseEntity
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int CouchId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        //public string SportsName { get; set; }
+        //public int CouchName { get; set; }
 
-        //lets start
+        public decimal Price { get; set; }
+        public int Capacity { get; set; }
+        public DateTime StartDate { get; set; }
+        public int DurationInDays { get; set; }
+
+        //relations
+
+        public virtual User Coach { get; set; }
+        public long UserId { get; set; }
+        //این رابطه یک به یک با یوزر مربوط به رابطه یک به یک بین مدرس و دوره است
+
+        public  ICollection<UserInCourse> customers { get; set; }
+        // اینم رابطه یک به چند بین دوره و یوزر ها (مشتریان) هست
+
+        public virtual Sport Sport { get; set; }
+        public long SportId { get; set; }
+        //ما یعنی "دوره" با واسطه به کتگوری مرتبط هستیم 
+        // یعنی از طریق اسپورت پس نیازی به رابطه مستقیم با کتگوری نیست. در واقع توصیه نمیشه
+
+
+        
+    }
+
+
+    // ----------- sport------//
+
+    public class Sport : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        //relations
+        public ICollection<Course> Courses { get; set; }
+
+        public virtual Category Category { get; set; }
+        public long CategoryId { get; set; }
+    }
+
+
+    //--------Category--//
+
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; }
+
+        //relations
+        public virtual Category ParentCategory { get; set; }
+        public long? ParentId { get; set; }
+
+        public ICollection<Category> SubCategories { get; set; }
+
+
+        //public ICollection<Course> Courses { get; set; }
+        
+        public ICollection<Sport> Sports { get; set; }
+    }
+
+    //----UserInCourse---//
+    public class UserInCourse : BaseEntity
+    {
+        //relation
+        public virtual Course Course { get; set; }
+        public long CourseId { get; set; }
+
+        public virtual User customer { get; set; }
+        public long UserId { get; set; }
+        
     }
 }
 
