72f54e9eb3e9829340afacacdc4eefa39bc3d923
rezanaim <reza.naim1380@gmail.com>
2025-11-24 15:43:40 +0330

    update Course

--- GymTest.Domain/Entities/CourseNmore/Course.cs
+++ GymTest.Domain/Entities/CourseNmore/Course.cs
@@ -18,7 +18,7 @@ namespace GymTest.Domain.Entities.CourseNmore
         public decimal Price { get; set; }
         public int Capacity { get; set; }
         //public DateTime StartDate { get; set; }
-        public int DurationInDays { get; set; }
+        public int DurationInHours { get; set; }
 
         //relations
 
