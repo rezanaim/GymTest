ac7880a203eef1d9b5d310f59935af25c7b3cc4d
rezanaim <reza.naim1380@gmail.com>
2025-11-24 16:44:40 +0330

    Add Category Seed

---
+++ Common/CategorySeed.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymTest.Common
+{
+    public class CategorySeed
+    {
+        public const String Ball = "Ball Sports";
+        public const String Mind = "Mind Sports";
+        public const String Fight = "Fight Sports";
+        public const String Winter = "Winter Sports";
+
+    }
+}
