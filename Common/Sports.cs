c8420051167f40b22c88e9e0f18ae1e13756a886
rezanaim <reza.naim1380@gmail.com>
2025-11-24 16:27:48 +0330

    sports in GymTest.common

---
+++ Common/Sports.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymTest.Common
+{
+    class Sports
+    {
+        //توپی
+        public const string Football = "Football";
+        public const string Pingpong = "Pingpong";
+
+        //فکری
+        public const string Chess = "Chess";
+
+        //رزمی
+        public const string Wrestling = "Wrestling";
+        public const string Boxing = "Boxing";
+
+        //اسکی
+        public const string Ski = "Ski";
+
+    }
+}
