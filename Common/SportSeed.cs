e60ce01527b3f39d76effdb4e859c740fbf89c66
rezanaim <reza.naim1380@gmail.com>
2025-11-24 16:32:34 +0330

    public SportSeed

---
+++ Common/SportSeed.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymTest.Common
+{
+    public class SportSeed
+    {
+        //توپی
+        public const string Football = "Football";
+        public const string Pingpong = "Pingpong";
+
+        //فکری
+        public const string Chess = "Chess";
+
+        //رزمی
+        public const string Wrestling = "Wrestling";
+        public const string Boxing = "Boxing";
+
+        //اسکی
+        public const string Ski = "Ski";
+
+    }
+}
