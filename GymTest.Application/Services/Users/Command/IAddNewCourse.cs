260a0f1e6f93fb27bed711b78e967e7fb400f85a
rezanaim <reza.naim1380@gmail.com>
2025-11-25 15:22:30 +0330

    Add New Course service

--- GymTest.Application/Services/Users/Command/IAddNewCourse.cs
+++ GymTest.Application/Services/Users/Command/IAddNewCourse.cs
@@ -1,6 +1,8 @@
 ﻿using Common;
 using GymTest.Application.Interfaces.Contexts;
 using GymTest.Domain.Entities.CourseNmore;
+using GymTest.Domain.Entities.Users;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +13,7 @@ namespace GymTest.Application.Services.Users.Command
 {
     public interface IAddNewCourse
     {
-        ResultDto<ResultAddNewCourseDto> Execute(RequestAddNewCourseDto request);
+        ResultDto<ResultAddNewCourseDto> Execute(RequestAddNewCourseDto request, long coachId);
     }
 
     public class AddNewCourse : IAddNewCourse
@@ -23,7 +25,7 @@ namespace GymTest.Application.Services.Users.Command
             _context = context;
         }
 
-        public ResultDto<ResultAddNewCourseDto> Execute(RequestAddNewCourseDto request)
+        public ResultDto<ResultAddNewCourseDto> Execute(RequestAddNewCourseDto request, long coachId)
         {
             if (string.IsNullOrWhiteSpace(request.Title))
             {
@@ -35,7 +37,7 @@ namespace GymTest.Application.Services.Users.Command
                     },
                     IsSuccess = false,
                     Message = "لطفا عنوان دوره را وارد کنید"
-                    
+
                 };
             }
 
@@ -62,6 +64,55 @@ namespace GymTest.Application.Services.Users.Command
                 SportId = request.SportId
             };
 
+            //اگه طرف رول کوچ نداشت باید بهش اضافه کنیم
+
+            //var theCoach = _context.Users.Find(coachId);
+
+
+
+            //IQueryable<User>
+            IQueryable<User> queryUser = _context.Users
+                .Include(u => u.UserInRoles)
+                .ThenInclude(u => u.Role);
+
+            var theCoach = queryUser.FirstOrDefault(u => u.Id == coachId);
+
+            if (theCoach == null)
+            {
+                return new ResultDto<ResultAddNewCourseDto>()
+                {
+                    Data = new ResultAddNewCourseDto()
+                    {
+                        CourseId = 0,
+                    },
+                    IsSuccess = false,
+                    Message = "سازنده دوره یافت نشد. اول ثبت نام کنید"
+                };
+            }
+
+            var temp = theCoach.UserInRoles;
+
+            var roles = temp.Select(r => r.RoleId).ToList();
+
+            var coachRole = _context.Roles.FirstOrDefault(r => r.Name == "Coach");
+            if (coachRole == null)
+            {
+                return new ResultDto<ResultAddNewCourseDto>()
+                {
+                    Data = new ResultAddNewCourseDto()
+                    {
+                        CourseId = 0,
+                    },
+                    IsSuccess = false,
+                    Message = "رول Coach ایجاد نشده است"
+                };
+            }
+            var coachRoleId = coachRole.Id;
+            if (!roles.Contains(coachRoleId))
+            {
+                _context.UserInRoles.Add(new UserInRole { UserId = coachId, RoleId = coachRoleId });
+            }
+
             _context.Courses.Add(newCourse);
             _context.SaveChanges();
 
@@ -73,23 +124,21 @@ namespace GymTest.Application.Services.Users.Command
                 },
                 IsSuccess = true,
                 Message = $"دوره{newCourse.Title} با موفقیت ایجاد شد"
-                
+
             };
 
             return (result);
         }
     }
 
-
-
     public class RequestAddNewCourseDto
     {
         public string Title { get; set; }
         public string Description { get; set; }
         public decimal price { get; set; }
-        public long CoachId {get;set;}
+        public long CoachId { get; set; }
         public long SportId { get; set; }
-        
+
 
     }
 
@@ -97,4 +146,6 @@ namespace GymTest.Application.Services.Users.Command
     {
         public long CourseId { get; set; }
     }
+
+
 }
