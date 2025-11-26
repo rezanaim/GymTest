3d8e6555b600934312053c4c9f3f83eda38c1869
rezanaim <reza.naim1380@gmail.com>
2025-11-26 12:51:40 +0330

    Async/await ed + remove comments

--- GymTest.Application/Services/Users/Command/ILoginUser.cs
+++ GymTest.Application/Services/Users/Command/ILoginUser.cs
@@ -13,7 +13,7 @@ namespace GymTest.Application.Services.Users.Command
 {
     public interface ILoginUser
     {
-        ResultDto<ResultLoginUserDto>Execute(RequestLoginUserDto request);
+        Task<ResultDto<ResultLoginUserDto>>Execute (RequestLoginUserDto request);
     }
 
     public class LoginUser : ILoginUser
@@ -27,10 +27,10 @@ namespace GymTest.Application.Services.Users.Command
             _context = context;
         }
 
-        public ResultDto<ResultLoginUserDto> Execute(RequestLoginUserDto request)
+        public async Task <ResultDto<ResultLoginUserDto>> Execute(RequestLoginUserDto request)
         {
 
-            // اگه خالی بودن بیاد ارور بده که پرش کن
+
             if (string.IsNullOrWhiteSpace(request.Email))
             {
                 return new ResultDto<ResultLoginUserDto>()
@@ -60,16 +60,14 @@ namespace GymTest.Application.Services.Users.Command
 
 
 
-            // اگه نبودن که بیا یه یوزر بساز و برو از دیتا بیس حایی مع اینیل ایمیل وارد شدس
-            // و کاربر اکتیوه و... رو فرست اور دیفالت کن
-            // اگه یوزر نال بود که بیا بیا ارور بده بگو ثبت نام نکردی
+
 
             
-            var user = _context.Users
+            var user = await _context.Users
                 .Include(p => p.UserInRoles)
                 .ThenInclude(p => p.Role)
                 .Where(p => p.Email.Equals(request.Email)
-                && p.IsActive == true).FirstOrDefault();
+                && p.IsActive == true).FirstOrDefaultAsync();
             
 
             if (user == null)
@@ -80,40 +78,11 @@ namespace GymTest.Application.Services.Users.Command
                     Message = "کاربری با این مشخصات یافت نشد",
                     Data = new ResultLoginUserDto()
                     {
-                        //UserId = 0,
+
                     }
                 };
             }
 
-            //اگه اوکی بود بیا  چک ک ایا پسورد وارد شده درسته یا نه
-            // اگه نبود دوباره ارور که رمزت ریده
-
-
-
-            /*
-            var passwordHasher = new PasswordHasher();
-            var hashedRequestPassword = passwordHasher.HashPassword(request.Password);
-            
-            if (hashedRequestPassword != user.Password)
-            {
-                return new ResultDto<ResultLoginUserDto>()
-                {
-                    IsSuccess = false,
-                    Message = "پسورد وارد شده اشتباه است",
-                    Data = new ResultLoginUserDto()
-                    {
-                        //UserId = 0,
-                    }
-                };
-            }
-            */
-
-            // چک کردن پسورد به شکل بالا اشتباهه. دلیل؟
-            // چون ما از رمزنگاری کاستوم و خاصی استفاده کردیم
-            // برخلاف بعضی روش های ساده. اگر ما یک متن ایکس رو به عنوان پسورد در نظر بگیریم
-            // هش شده ایکس.  برابر با  هش شده ایکس نیست
-            // با این که ماده اولیه یکیه ولی هر بار هش کنی یه هش جدید و یونیک برات میسازیه ازش
-            // راه هل؟ متد وریفای خودش
 
             var passwordHasher = new PasswordHasher();
             var vefifyPassword = passwordHasher.VerifyPassword(user.Password, request.Password);
@@ -126,13 +95,11 @@ namespace GymTest.Application.Services.Users.Command
                     Message = "پسورد وارد شده اشتباه است",
                     Data = new ResultLoginUserDto()
                     {
-                        //UserId = 0,
+
                     }
                 };
             }
 
-            // اگه اوک بود
-            // بیا یه ور رول تعریف کن که یه استرینگ خالیه بعد با یه فوریچ
             // بیا و تک تک ایتم هارو به ازای رول های یوزر اد کن توی وریبل رول
 
 
@@ -157,12 +124,9 @@ namespace GymTest.Application.Services.Users.Command
                 }
             };
 
-            // خروجی بده که اوکی شد رول و یوزر ای دی و نیمش هم به عنوان دیتا میدیم
-            // کلاس ریزالت یوزر لاگینم که قبلا گذاشتم
+
             // در مورد کلیم ها هم توی کنترولر هندلش میکنیم
 
-            //temp return null
-            //return null;
         }
 
         public class RequestLoginUserDto
