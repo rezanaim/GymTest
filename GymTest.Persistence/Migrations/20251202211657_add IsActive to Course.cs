ad57414fd00c061bef47443ea826e173255a502a
rezanaim <reza.naim1380@gmail.com>
2025-12-03 01:44:14 +0330

    Add IsActive to Course

---
+++ GymTest.Persistence/Migrations/20251202211657_add IsActive to Course.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GymTest.Persistence.Migrations
+{
+    public partial class addIsActivetoCourse : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Courses",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Courses");
+        }
+    }
+}
