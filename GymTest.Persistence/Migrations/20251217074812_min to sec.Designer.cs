672b699f823aadfb3c9a25b485a4866f1fb62662
rezanaim <reza.naim1380@gmail.com>
2025-12-17 11:19:00 +0330

    Duratin Min to Sec

--- GymTest.Persistence/Migrations/20251217074812_min to sec.Designer.cs
+++ GymTest.Persistence/Migrations/20251217074812_min to sec.Designer.cs
@@ -4,14 +4,16 @@ using GymTest.Persistence.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GymTest.Persistence.Migrations
 {
     [DbContext(typeof(DataBaseContext))]
-    partial class DataBaseContextModelSnapshot : ModelSnapshot
+    [Migration("20251217074812_min to sec")]
+    partial class mintosec
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GymTest.Persistence/Migrations/20251217074812_min to sec.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GymTest.Persistence.Migrations
+{
+    public partial class mintosec : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DurationInMins",
+                table: "Courses",
+                newName: "DurationInSecs");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DurationInSecs",
+                table: "Lectures",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DurationInSecs",
+                table: "Lectures");
+
+            migrationBuilder.RenameColumn(
+                name: "DurationInSecs",
+                table: "Courses",
+                newName: "DurationInMins");
+        }
+    }
+}
